65cc8fcff309e21ba34824a31c0a7cdc459025e1
imrabti <imrabti@gmail.com>
2013-03-05 18:18:14 +0000

    Ajout du trie par annee dans le niveau d'etude.

--- src/main/java/com/gsr/myschool/server/repos/NiveauEtudeRepos.java
+++ src/main/java/com/gsr/myschool/server/repos/NiveauEtudeRepos.java
@@ -19,8 +19,5 @@ package com.gsr.myschool.server.repos;
 import com.gsr.myschool.server.business.core.NiveauEtude;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface NiveauEtudeRepos extends JpaRepository<NiveauEtude, Long> {
-    List<NiveauEtude> findByFiliereId(Long id);
 }
