a3afdff1378c919316d11caf532f343886791756
gjnkouyee <gael.nkouyee@gmail.com>
2013-03-12 19:41:32 +0000

    Fixes on Reports and Export

--- src/main/java/com/gsr/myschool/common/client/request/ExcelRequestBuilder.java
+++ src/main/java/com/gsr/myschool/common/client/request/ExcelRequestBuilder.java
@@ -17,7 +17,11 @@
 package com.gsr.myschool.common.client.request;
 
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.http.client.*;
+import com.google.gwt.http.client.Request;
+import com.google.gwt.http.client.RequestBuilder;
+import com.google.gwt.http.client.RequestCallback;
+import com.google.gwt.http.client.RequestException;
+import com.google.gwt.http.client.Response;
 import com.google.gwt.user.client.Window;
 import com.google.web.bindery.autobean.shared.AutoBean;
 import com.google.web.bindery.autobean.shared.AutoBeanCodex;
@@ -39,7 +43,7 @@ public class ExcelRequestBuilder extends RequestBuilder {
             @Override
             public void onResponseReceived(Request request, Response response) {
                 if (GWT.isScript()) {
-                    Window.open("resource/excel?fileName=" + response.getText(), "_blank", "");
+                    Window.open("/preinscription/resource/excel?fileName=" + response.getText(), "_blank", "");
                 } else {
                     Window.open("/resource/excel?fileName=" + response.getText(), "_blank", "");
                 }
