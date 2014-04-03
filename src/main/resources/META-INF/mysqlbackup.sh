eed107ac539446e0cf4a3968228c3cd256d40bd6
Kecha mohamed <kecha.mohamed@gmail.com>
2014-04-03 23:44:42 +0100

    sh changes

--- src/main/resources/META-INF/mysqlbackup.sh
+++ src/main/resources/META-INF/mysqlbackup.sh
@@ -2,8 +2,6 @@
 
 DAY=`/bin/date +%Y%m%d_%H_%M_%S`
 
-/opt/bitnami/mysql/bin/mysqldump -u root -pbitnami myschool | gzip > /home/bitnami/db_saves/myschool_$DAY.sql.gz
+/opt/mysql/bin/mysqldump -umyschool -pmyschool myschool | gzip > /home/serveradmin/db_saves/myschool_$DAY.sql.gz
 
-/opt/bitnami/mysql/bin/mysql -u root -pbitnami     myschool    < /home/bitnami/db_saves/gsr-all-major-10-03-2013-V0.sql
-
-
+/opt/mysql/bin/mysql -umyschool -pmyschool myschool < /home/serveradmin/db_saves/gsr-all-major-10-03-2013-V0.sql
