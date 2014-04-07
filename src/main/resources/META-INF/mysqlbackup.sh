a578667aacd77cb59b673ebd4522bf4eaa024d28
Kecha Mohamed <kecha.mohamed@gmail.com>
2014-04-07 08:36:17 +0000

    verification of paths

--- src/main/resources/META-INF/mysqlbackup.sh
+++ src/main/resources/META-INF/mysqlbackup.sh
@@ -2,6 +2,6 @@
 
 DAY=`/bin/date +%Y%m%d_%H_%M_%S`
 
-/opt/mysql/bin/mysqldump -umyschool -pmyschool myschool | gzip > /home/serveradmin/db_saves/myschool_$DAY.sql.gz
+/usr/bin/mysqldump -umyschool -pmyschool myschool | gzip > /home/serveradmin/db_dumps/myschool_$DAY.sql.gz
 
-/opt/mysql/bin/mysql -umyschool -pmyschool myschool < /home/serveradmin/db_saves/gsr-all-major-10-03-2013-V0.sql
+/usr/bin/mysql -umyschool -pmyschool myschool < /home/serveradmin/db_dumps/gsr-all-major-10-03-2013-V0.sql
